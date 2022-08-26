0c07a3828266720bbb2f98fe193674ed509aba91
Sanjukta <jinu.nayak@gmail.com>
2022-08-26 18:00:29 +0530

    push

---
+++ push_to_git.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\biswa\OneDrive\Documents\Python Tutorial\Streamlite\NeuroHack"
+
+call git init && git add . && git commit -m "update" && git remote set-url origin https://github.com/SanjuktaBiswal/oddlogic.git && git push origin master --force
+
