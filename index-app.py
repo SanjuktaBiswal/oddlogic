3940b22c38a2210af3fb1d84893ed3a9542b3cc5
Sanjukta <36503064+SanjuktaBiswal@users.noreply.github.com>
2022-08-31 13:56:16 +0530

    Update index-app.py

--- index-app.py
+++ index-app.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 import streamlit.components.v1 as components
 import plotly.graph_objects as go
 from  multipage import MultiPage
-import login,signup,settings,prediction_tnsr#,prediction,training,report,transfer
+import login,signup,prediction_tnsr,report,prediction,training,transfersettings
 from PIL import  Image
 
 
